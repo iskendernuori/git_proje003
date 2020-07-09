438af2ab1c9cc1bf85c4c33933c9014dc890350c
iskendernuori <isknuori@gmail.com>
2020-07-09 12:54:50 +0300

    ucuncu commit yapıldı

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -5,6 +5,9 @@ public class ClassOne {
 	public static void main(String[] args) {
 	System.out.println("Local master branch");
 	System.out.println("Local ikinci komut master dan");
+	
+	System.out.println("Local masterdan 2. commit");
+	System.out.println("Local masterdan 2. commit");
 
 	}
 
