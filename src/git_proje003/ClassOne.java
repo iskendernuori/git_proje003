b53cf3efeba4b6b1338388cba0f6b98f0fba7532
iskendernuori <66529044+iskendernuori@users.noreply.github.com>
2020-07-09 12:53:50 +0300

    Update ClassOne.java
    degişiklikler remote masterdan yapılmıştır

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -5,6 +5,10 @@ public class ClassOne {
 	public static void main(String[] args) {
 	System.out.println("Local master branch");
 	System.out.println("Local ikinci komut master dan");
+		
+		System.out.println("Local master branch");
+	System.out.println("Local ikinci komut master dan");
+		
 
 	}
 
